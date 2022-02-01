2cf9321f23ab76d245495bb5bda0ee0379045b96
Leon Lovett <1970leonlovett@gmail.com>
2022-01-31 19:33:51 -0600

    added data to table

--- src/js/myscript.js
+++ src/js/myscript.js
@@ -1,7 +1,42 @@
 console.log('Welcome to HackerU-JustCode Assignment');
-$(document).ready(function() {
+$(document).ready(function () {
     getRemoteData();
 });
-function getRemoteData() {
-    // write your code here to fetch data from api.
+
+const dataUrl = 'https://www.justcode.com/dataservice/api/laptopManufacturer/list';
+let data;
+async function getRemoteData() {
+    data = await (await fetch(dataUrl)).json();
+    renderDataInTable();
+}
+
+function renderDataInTable() {
+    // Get a reference to the tbody element
+    const tBody = document.querySelector('tbody');
+    data.forEach(item => {
+        const row = createRow(item);
+        tBody.appendChild(row);
+    });
+}
+
+function createRow(rowData) {
+    const row = document.createElement('tr');
+
+    // columns
+    const id = document.createElement('td');
+    id.innerText = rowData.id;
+    const name = document.createElement('td');
+    name.innerText = rowData.manufacturerName;
+    const ceo = document.createElement('td');
+    ceo.innerText = rowData.nameOfCEO;
+    const headquarters = document.createElement('td');
+    headquarters.innerText = rowData.headquarters;
+    const modifiedOn = document.createElement('td');
+    modifiedOn.innerText = rowData.modifiedOn;
+    row.appendChild(id);
+    row.appendChild(name);
+    row.appendChild(ceo);
+    row.appendChild(headquarters);
+    row.appendChild(modifiedOn);
+    return row;
 }
