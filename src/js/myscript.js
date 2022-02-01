035c8b71785d35c812a75a8a2de982b2efcf5af7
Leon Lovett <1970leonlovett@gmail.com>
2022-01-31 19:40:15 -0600

    refactor code to conform some mysterious predefined expected format

--- src/js/myscript.js
+++ src/js/myscript.js
@@ -7,36 +7,25 @@ const dataUrl = 'https://www.justcode.com/dataservice/api/laptopManufacturer/lis
 let data;
 async function getRemoteData() {
     data = await (await fetch(dataUrl)).json();
-    renderDataInTable();
-}
-
-function renderDataInTable() {
-    // Get a reference to the tbody element
     const tBody = document.querySelector('tbody');
     data.forEach(item => {
         const row = createRow(item);
+        const row = document.createElement('tr');
+        const id = document.createElement('td');
+        id.innerText = rowData.id;
+        const name = document.createElement('td');
+        name.innerText = rowData.manufacturerName;
+        const ceo = document.createElement('td');
+        ceo.innerText = rowData.nameOfCEO;
+        const headquarters = document.createElement('td');
+        headquarters.innerText = rowData.headquarters;
+        const modifiedOn = document.createElement('td');
+        modifiedOn.innerText = rowData.modifiedOn;
+        row.appendChild(id);
+        row.appendChild(name);
+        row.appendChild(ceo);
+        row.appendChild(headquarters);
+        row.appendChild(modifiedOn);    
         tBody.appendChild(row);
     });
-}
-
-function createRow(rowData) {
-    const row = document.createElement('tr');
-
-    // columns
-    const id = document.createElement('td');
-    id.innerText = rowData.id;
-    const name = document.createElement('td');
-    name.innerText = rowData.manufacturerName;
-    const ceo = document.createElement('td');
-    ceo.innerText = rowData.nameOfCEO;
-    const headquarters = document.createElement('td');
-    headquarters.innerText = rowData.headquarters;
-    const modifiedOn = document.createElement('td');
-    modifiedOn.innerText = rowData.modifiedOn;
-    row.appendChild(id);
-    row.appendChild(name);
-    row.appendChild(ceo);
-    row.appendChild(headquarters);
-    row.appendChild(modifiedOn);
-    return row;
 }
